34de1e86f99745099e04ceb0f1e4594189224572
indianaMitko62 <indiana.mitko@gmail.com>
2024-08-05 15:17:22 +0300

    Added frame decoding implementation

--- C/flatptp.c
+++ C/flatptp.c
@@ -73,22 +73,98 @@ size_t hdlc_encode_data(uint8_t address, int8_t *data, size_t data_size, int8_t 
     return frame_size;
 }
 
-void print_frame(int8_t *frame, size_t buf_size)
+void hdlc_decode_start(hdlc_decode_ctx_t *ctx, int8_t *data, uint16_t max_size)
+{
+    ctx->data = data;
+    ctx->buf_max_size = max_size;
+    ctx->data_index = 0;
+    ctx->in_frame = 0;
+}
+
+void hdlc_decode_eat(hdlc_decode_ctx_t *ctx, int8_t b)
+{
+    if (FLAG == b)
+    {
+        ctx->in_frame = !ctx->in_frame;
+        return;
+    }
+    else
+    {
+        if (ctx->in_frame)
+        {
+            switch (ctx->data_index)
+            {
+            case 0:
+                ctx->address = b;
+                break;
+            case 1:
+                ctx->ctl->receive_sequence_number = b & 0xE0;
+                ctx->ctl->poll_flag_bit = b & 0x10;
+                ctx->ctl->send_sequence_number = b & 0x0E;
+                ctx->ctl->type = b & 0x01;
+                break;
+            default:
+                ctx->data[ctx->data_index++] = b;
+                break;
+            }
+        }
+    }
+    if (ctx->data_index + 2 == ctx->buf_max_size)
+    {
+        return; // return error for not enough size for data
+    }
+}
+
+uint16_t hdlc_decode_has_complete_frame(hdlc_decode_ctx_t *ctx)
+{
+    if (ctx->in_frame)
+    {
+        return 0;
+    }
+    ctx->crc = ctx->data[ctx->data_index - 1] << 8 | ctx->data[ctx->data_index - 2];
+    if (ctx->crc == crc16_ccitt(ctx->data, ctx->data_index - 2))
+    {
+        return ctx->data_index - 2; // removing CRC from data buffer.
+    }
+    else
+    {
+        return 0; // return wrong FCS
+    }
+}
+
+void print_encoded_frame(int8_t *frame, size_t frame_size)
 {
     int i;
     printf("Flag:\t0x%02X\n", frame[0]);
     printf("Addr:\t0x%02X\n", frame[1]);
     printf("Ctrl:\t0x%02X\n", frame[2]);
-    printf("\tRecN:\t%d\n", (frame[2] >> 5) & 0x07);
-    printf("\tP/Fb:\t%d\n", (frame[2] >> 4) & 0x01);
-    printf("\tSenN:\t%d\n", (frame[2] >> 1) & 0x07);
+    printf("\tRecN:\t%d\n", frame[2] & 0xE0);
+    printf("\tP/Fb:\t%d\n", frame[2] & 0x10);
+    printf("\tSenN:\t%d\n", frame[2] & 0x0E);
     printf("\tType:\t%d\n", frame[2] & 0x01);
     printf("Data:\n");
-    for (i = 3; i < buf_size - 3; i++)
+    for (i = 3; i < frame_size - 3; i++)
     {
         printf("0x%02X, ", frame[i]);
     }
     printf("\nEnd of Data\n");
     printf("FCS:\t0x%02X%02X\n", (uint8_t)frame[i], (uint8_t)frame[i + 1]);
-    printf("Flag:\t0x%X\n\n", frame[buf_size - 1]);
+    printf("Flag:\t0x%X\n\n", frame[frame_size - 1]);
+}
+
+void print_decoded_frame_ctx(hdlc_decode_ctx_t *ctx)
+{
+    printf("Addr:\t0x%02X\n", ctx->address);
+    printf("Ctrl:\t0x%02X\n", ctx->ctl);
+    printf("\tRecN:\t%d\n", ctx->ctl->receive_sequence_number);
+    printf("\tP/Fb:\t%d\n", ctx->ctl->poll_flag_bit);
+    printf("\tSenN:\t%d\n", ctx->ctl->send_sequence_number);
+    printf("\tType:\t%d\n", ctx->ctl->type);
+    printf("Data:\n");
+    for (int i = 0; i < ctx->data_index - 2; i++)
+    {
+        printf("0x%02X, ", ctx->data[i]);
+    }
+    printf("\nEnd of Data\n");
+    printf("FCS:\t0x%02X%02X\n", ctx->crc);
 }
--- C/flatptp.h
+++ C/flatptp.h
@@ -8,6 +8,7 @@ typedef struct
     uint8_t receive_sequence_number;
     uint8_t send_sequence_number;
     uint8_t poll_flag_bit;
+    uint8_t type;
 } hdlc_encode_ctl_t;
 
 // hdlc_encode_data encodes the data stored in data as a HDLC frame and sends the frame using the user provided send_bytes function
@@ -18,16 +19,21 @@ size_t hdlc_encode_data(uint8_t address, int8_t *data, size_t data_size, int8_t 
 // *********************
 typedef struct
 {
-    char *buf;
-    uint16_t max_size;
-    // and other stuff
+    size_t buf_max_size;
+    uint8_t in_frame;
+    size_t data_index;
+
+    uint8_t address;
+    hdlc_encode_ctl_t *ctl;
+    int8_t *data;
+    uint16_t crc;
 } hdlc_decode_ctx_t;
 
-void hdlc_decode_start(hdlc_decode_ctx_t *ctx, char *buf, uint16_t max_size);
-void hdlc_decode_eat(hdlc_decode_ctx_t *ctx, char c);
+void hdlc_decode_start(hdlc_decode_ctx_t *ctx, int8_t *data, uint16_t max_size);
+void hdlc_decode_eat(hdlc_decode_ctx_t *ctx, int8_t c);
 
 // if the previous eat() call ate the last byte of a complete frame,
-// this function should return the size of the frame (and buf should
+// this function should return the size of the frame (and data field should
 // contain the decoded frame content)
 uint16_t hdlc_decode_has_complete_frame(hdlc_decode_ctx_t *ctx);
 uint16_t hdlc_decode_has_error(hdlc_decode_ctx_t *ctx);
@@ -35,4 +41,5 @@ uint16_t hdlc_decode_has_error(hdlc_decode_ctx_t *ctx);
 // *********************
 // HDLC help
 // *********************
-void print_frame(int8_t *frame, size_t buf_size);
+void print_encoded_frame(int8_t *frame, size_t buf_size);
+void print_decoded_frame_ctx(hdlc_decode_ctx_t *ctx);
