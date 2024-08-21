20e18aab13f2ec8a41f9bdbb5c670e99ce309606
indianaMitko62 <indiana.mitko@gmail.com>
2024-08-21 16:51:58 +0300

    improving example for receiving messages
    added packet loss calculation

--- examples/C/receiver/src/receiver.c
+++ examples/C/receiver/src/receiver.c
@@ -6,21 +6,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int8_t receive_byte(int8_t *buf)
+#define TEST_MSG_CNT 10
+
+int8_t receive_byte(int8_t *buf, int fd)
 {
-    int fd = open("/dev/ttyACM0", O_RDONLY);
-    if (0 > fd)
-    {
-        printf("could not open port\n");
-        return 1;
-    }
     size_t bytes_read = read(fd, buf, 1);
     if (0 >= bytes_read)
     {
         printf("could not read from port\n");
         return 2;
     }
-    close(fd);
     return 0;
 }
 
@@ -28,33 +23,46 @@ int main()
 {
     char buf[256];
     int8_t c;
-    hdlc_decode_ctx_t *decoder;
-    hdlc_decode_start(decoder, buf, sizeof(buf) - 1); // -1 to allow adding terminating zeros for easy printing
-    while (true)
+    hdlc_decode_ctx_t decoder = hdlc_decode_start(buf, sizeof(buf) - 1); // -1 to allow adding terminating zeros for easy printing
+    int fd = open("/dev/ttyACM0", O_RDONLY);
+    if (0 > fd)
     {
-        // sleep(1);
-        int err = receive_byte(&c);
-        if (err)
-        {
-            printf("Error receiving byte. Error code: %d\n", err);
-            continue;
-        }
-        int res = hdlc_decode_eat(decoder, c);
-        if (res > 0)
-        {
-            printf("message received: ");
-            for (int i = 0; i < res; i++)
-            {
-                printf("%c", decoder->data[i]);
-            }
-            printf("\n");
-            continue;
-        }
-        if (res != INFO_BYTE_EATHEN)
-        {
-            printf("Error eating byte 0x%02X: %d", c, res);
-        }
-        printf("Sucessfully eaten 0x%02X: %d", c, res);
+        printf("could not open port\n");
+        return 1;
     }
+    uint32_t successful_messages = 0;
+    for (int i = 0; i < TEST_MSG_CNT; i++)
+    {
+        while (true)
+        {
+            int err = receive_byte(&c, fd);
+            if (err)
+            {
+                printf("Error receiving byte. Error code: %d\n", err);
+                continue;
+            }
+            int res = hdlc_decode_eat(&decoder, c);
+            if (res > 0)
+            {
+                printf("message received: '");
+                for (int i = 0; i < res; i++)
+                {
+                    printf("%c", decoder.data[i]);
+                }
+                printf("'\n");
+                successful_messages++;
+                break;
+            }
+            if (ERR_INVALID_FRAME == res)
+                break;
+            if (INFO_BYTE_EATHEN != res)
+            {
+                printf("Error eating byte 0x%02X\t%c: %d\n\n\n", c, c, res);
+                continue;
+            }
+        }
+    }
+    printf("\n\n\nTransmitted messages: %d\tSuccessfully received messages: %d\t%f%% packet loss\n", TEST_MSG_CNT, successful_messages, (1 - (float)successful_messages / TEST_MSG_CNT) * 100);
+    close(fd);
     return 0;
 }
